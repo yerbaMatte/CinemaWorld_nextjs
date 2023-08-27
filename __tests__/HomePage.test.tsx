60c7884e51359f6846168958d29b9a57b99b60cf
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-08-27 19:49:22 +0200

    react testing library firebase

--- __tests__/HomePage.test.tsx
+++ __tests__/HomePage.test.tsx
@@ -1,18 +1,10 @@
-import Home from '@/app/page';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { getAuth } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
+import { Add } from '@/math';
 
-// Manually configure Firebase for testing
-const testFirebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // Replace with your test API key
-  // Other configuration options...
-};
-const testFirebaseApp = initializeApp(testFirebaseConfig);
-const testAuth = getAuth(testFirebaseApp);
+// Mocking Add function
+const mockAddFunction = jest.fn();
 
 describe('Home Page - Rendering', () => {
-  it('should have hero header text');
-  render(<Home />);
-  screen.getByText('Gladiator');
+  it('hello', () => {});
 });
---
+++ math.tsx
@@ -0,0 +1,3 @@
+export const Add = (arg1: number, arg2: number): number => {
+  return arg1 + arg2;
+};
